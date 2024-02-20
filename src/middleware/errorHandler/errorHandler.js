5862d37ba56bd0282bd54e78fdb3fe15b41e6243
Dongwon KIm <dongwon000103@gmail.com>
2024-02-20 14:50:15 +0900

    fix: error handler request header sent err

--- src/middleware/errorHandler/errorHandler.js
+++ src/middleware/errorHandler/errorHandler.js
@@ -2,13 +2,13 @@ import { ApplicationError } from '../../util/error/applicationError.js';
 
 const errorHandler = (err, req, res, next) => {
     if (err instanceof ApplicationError) {
-        res.status(err.status).json({
+        return res.status(err.status).json({
             success: false,
             message: err.message,
         });
     }
 
-    res.status(500).json({
+    return res.status(500).json({
         success: false,
         message: "Can't Handling Error",
     });
