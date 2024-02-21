6f6a55e30a533fd2071e1aaf35d67b2283977eec
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-02-21 16:29:00 +0900

    feat:a QA 조회

--- src/router/qasRouter.js
+++ src/router/qasRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as QAservice from "../services/qaService.js";
-import { ApplicationError } from '../util/error/applicationError.js';
+import { ApplicationError } from "../util/error/applicationError.js";
 
 const router = express.Router();
 
@@ -18,5 +18,21 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-export default router;
+//req.user.id
 
+router.get("/:qaid", async function (req, res, next) {
+  try {
+    const qaId = req.params.qaid;
+    const qa = await QAservice.getQAById(qaId);
+    return res.status(200).json({
+      success: true,
+      message: "Successfully retrieved QA",
+      qa: qa,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+export default router;
--- src/service/qasService.js
+++ src/service/qasService.js
@@ -1,4 +1,5 @@
-import QAModel from '../db/models/qaModel.js';
+import QAModel from "../db/models/qaModel.js";
+import { ApplicationError } from "../util/error/applicationError.js";
 
 export const getAllQAs = async () => {
   try {
@@ -6,5 +7,22 @@ export const getAllQAs = async () => {
     return qas;
   } catch (error) {
     throw new ApplicationError(500, "Error retrieving QAs");
+  }
+};
+
+export const getQAById = async (qaId) => {
+  try {
+    let qa;
+    if (qaId) {
+      qa = await QAModel.findById(qaId).populate("userId", "nickname");
+      if (!qa) {
+        throw new ApplicationError(404, "QA not found");
+      }
+    } else {
+      throw new ApplicationError(400, "QA ID is required");
+    }
+    return qa;
+  } catch (error) {
+    throw new ApplicationError(500, "Error retrieving QA");
   }
 };
