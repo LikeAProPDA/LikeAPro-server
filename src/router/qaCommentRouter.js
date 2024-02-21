d3fa58296e098f93f8cfa1bb895f1e126627dd4e
chaeheonjeong <cogjs3210@naver.com>
2024-02-21 15:31:51 +0900

    feat: comment CRUD

--- src/router/qaCommentRouter.js
+++ src/router/qaCommentRouter.js
@@ -1,13 +1,18 @@
 import express from "express";
 import QAComment from "../db/models/QACommentModel";
-import { getComment, uploadComment } from "../service/qaCommentService";
+import {
+  getComment,
+  uploadComment,
+  updateComment,
+  deleteComment,
+} from "../service/qaCommentService";
 import { ApplicationError } from "../util/error/applicationError";
 const router = express.Router();
 
 // 댓글 조회 :  /api/qas/:qaId/comments
-router.get("/", async (req, res, next) => {
+router.get("/:qaId/comments", async (req, res, next) => {
   try {
-    const { qaId } = req.query;
+    const { qaId } = req.params;
     const qaComment = await getComment(qaId);
     res.status(200).json({
       success: true,
@@ -16,13 +21,13 @@ router.get("/", async (req, res, next) => {
     });
   } catch (err) {
     console.error(err);
-    next(new ApplicationError(400, "댓글을 조회할 수 없습니다."));
+    next(new ApplicationError(404, "댓글을 조회할 수 없습니다."));
   }
 });
 
 // 댓글 업로드 : /api/qas/:qaId/comments
 //loginRequired(임시 변수명): 로그인이 필요한 함수 구현 필요함.
-router.post("", loginRequired, async (req, res, next) => {
+router.post("/:qaId/comments", loginRequired, async (req, res, next) => {
   try {
     const { qaId } = req.query;
     const { content } = req.body;
@@ -34,12 +39,39 @@ router.post("", loginRequired, async (req, res, next) => {
     });
   } catch (err) {
     console.error(err);
-    next(new ApplicationError(400, "댓글 "));
+    next(new ApplicationError(404, "댓글을 작성할 수 없습니다."));
   }
 });
 
 // 댓글 업데이트 : /api/qas/comments/:commentId
-router.put("/:commentId", async (req, res, next) => {});
+router.put("/comments/:commentId", loginRequired, async (req, res, next) => {
+  try {
+    const { commentId } = req.params;
+    const { content } = req.body;
+    const updatedQaComment = await updateComment(commentId, content);
+    res.status(200).json({
+      success: true,
+      message: "댓글 수정 성공",
+      result: updatedQaComment,
+    });
+  } catch (err) {
+    console.error(err);
+    next(new ApplicationError(404, "댓글을 수정할 수 없습니다."));
+  }
+});
 
 // 댓글 삭제 : /api/qas/comments/:commentId
-router.delete("/:commentId", async (req, res, next) => {});
+router.delete("/comments/:commentId", loginRequired, async (req, res, next) => {
+  try {
+    const { commentId } = req.params;
+    const deletedComment = await deleteComment(commentId);
+    res.status(200).json({
+      success: true,
+      message: "댓글 삭제 성공",
+      result: deletedComment,
+    });
+  } catch (err) {
+    console.error(err);
+    next(new ApplicationError(404, "댓글을 삭제할 수 없습니다."));
+  }
+});
--- src/service/qaCommentService.js
+++ src/service/qaCommentService.js
@@ -3,20 +3,58 @@ import { ApplicationError } from "../util/error/applicationError";
 
 //댓글 가져오기
 const getComment = async (qaId) => {
-  const qaComment = await QACommentModel.find({
-    qaId: qaId,
-  });
-
-  return qaComment;
+  try {
+    const qaComment = await QACommentModel.find({
+      qaId: qaId,
+    });
+    return qaComment;
+  } catch (error) {
+    throw new ApplicationError(500, "댓글을 가져올 수 없습니다.", error);
+  }
 };
 
 //댓글 업로드
 const uploadComment = async (qaId, comment) => {
-  const qaComment = await QACommentModel.create({
-    qaId: qaId,
-    comment: comment,
-  });
-  return qaComment;
+  try {
+    const qaComment = await QACommentModel.create({
+      qaId: qaId,
+      comment: comment,
+    });
+    return qaComment;
+  } catch (error) {
+    throw new ApplicationError(500, "댓글을 업로드할 수 없습니다.", error);
+  }
 };
 
-export { getComment, uploadComment };
+//댓글 업데이트
+const updateComment = async (commentId, content) => {
+  try {
+    const foundComment = await QACommentModel.findById(commentId);
+    if (!foundComment) {
+      throw new ApplicationError(404, "해당 댓글을 찾을 수 없습니다.");
+    }
+
+    foundComment.content = content;
+    const updatedQaComment = await foundComment.save();
+    return updatedQaComment;
+  } catch (error) {
+    throw new ApplicationError(500, "댓글을 업데이트할 수 없습니다.", error);
+  }
+};
+
+//댓글 삭제
+const deleteComment = async (commentId) => {
+  try {
+    const deletedComment = await QACommentModel.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+      throw new ApplicationError(404, "해당 댓글을 찾을 수 없습니다.");
+    }
+
+    return deletedComment;
+  } catch (error) {
+    throw new ApplicationError(500, "댓글을 삭제할 수 없습니다.", error);
+  }
+};
+
+export { getComment, uploadComment, updateComment, deleteComment };
