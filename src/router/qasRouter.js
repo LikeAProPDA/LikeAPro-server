c163a119e1cf558c0c1fee9d6363a3fb7e3183fc
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-02-22 17:38:02 +0900

    QA 조회, 생성, 삭제, 수정 기능 구현

--- src/router/qasRouter.js
+++ src/router/qasRouter.js
@@ -2,9 +2,8 @@ import express from "express";
 import * as QAservice from "../service/qasService.js";
 import { ApplicationError } from "../util/error/applicationError.js";
 
-
 const router = express.Router();
-
+//모든 QA조회
 router.get("/", async function (req, res, next) {
   try {
     const qas = await QAservice.getAllQAs();
@@ -18,8 +17,7 @@ router.get("/", async function (req, res, next) {
     return next(error);
   }
 });
-
-
+//하나의 QA 조회
 router.get("/:qaid", async function (req, res, next) {
   try {
     const qaId = req.params.qaid;
@@ -34,6 +32,52 @@ router.get("/:qaid", async function (req, res, next) {
     return next(error);
   }
 });
+//추가
+router.post("/", async (req, res, next) => {
+  try {
+    const { title, content, author, isCompleted } = req.body;
+
+    // 새로운 QA 생성
+    const newQA = await QAservice.postQA(title, content, author, isCompleted);
+
+    res.status(201).json({
+      success: true,
+      message: "New QA created successfully.",
+      qa: newQA,
+    });
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+});
+//삭제
+router.delete("/:qaid", async function (req, res, next) {
+  try {
+    const qaId = req.params.qaid;
+    await QAservice.deleteQA(qaId);
+    return res.status(200).json({
+      success: true,
+      message: "QA deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+//수정
+router.put("/:qaid", async function (req, res, next) {
+  try {
+    const qaId = req.params.qaid;
+    const { title, content, isCompleted } = req.body;
+    await QAservice.editQA(qaId, { title, content, isCompleted });
+    return res.status(200).json({
+      success: true,
+      message: "QA updated successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
 export default router;
-
--- src/service/qasService.js
+++ src/service/qasService.js
@@ -1,23 +1,20 @@
-
-import QAModel from "../db/models/qaModel.js";
+import QA from "../db/models/qaModel.js";
 import { ApplicationError } from "../util/error/applicationError.js";
-
 
 export const getAllQAs = async () => {
   try {
-    const qas = await QAModel.find().populate("userId", "nickname");
+    const qas = await QA.find().populate("author", "nickname");
     return qas;
   } catch (error) {
     throw new ApplicationError(500, "Error retrieving QAs");
   }
 };
 
-
 export const getQAById = async (qaId) => {
   try {
     let qa;
     if (qaId) {
-      qa = await QAModel.findById(qaId).populate("userId", "nickname");
+      qa = await QA.findById(qaId).populate("author", "nickname");
       if (!qa) {
         throw new ApplicationError(404, "QA not found");
       }
@@ -26,7 +23,58 @@ export const getQAById = async (qaId) => {
     }
     return qa;
   } catch (error) {
+    console.log(error);
     throw new ApplicationError(500, "Error retrieving QA");
+  }
+};
+
+export const postQA = async (title, content, author, isCompleted) => {
+  try {
+    // 새로운 QA 생성 및 저장
+    const savedQA = await QA.create({
+      title: title,
+      content: content,
+      author: author,
+      isCompleted: isCompleted,
+    });
+
+    return savedQA;
+  } catch (error) {
+    throw new ApplicationError(500, "Error creating new QA");
+  }
+};
+
+export const deleteQA = async (qaId) => {
+  try {
+    if (!qaId) {
+      throw new ApplicationError(400, "QA ID is required");
+    }
+    const deletedQA = await QA.findByIdAndDelete(qaId);
+    if (!deletedQA) {
+      throw new ApplicationError(404, "QA not found");
+    }
+  } catch (error) {
+    if (error instanceof ApplicationError) {
+      throw error;
+    }
+    throw new ApplicationError(500, "Error deleting QA");
+  }
+};
+
+export const editQA = async (qaId, newData) => {
+  try {
+    if (!qaId) {
+      throw new ApplicationError(400, "QA ID is required");
+    }
+    const updatedQA = await QA.findByIdAndUpdate(qaId, newData, { new: true });
+    if (!updatedQA) {
+      throw new ApplicationError(404, "QA not found");
+    }
+  } catch (error) {
+    if (error instanceof ApplicationError) {
+      throw error;
+    }
+    throw new ApplicationError(500, "Error updating QA");
   }
 };
 
