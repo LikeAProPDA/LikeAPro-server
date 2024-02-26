7f52b96ac41b30b0faa4625ea951ca100f6a4015
chaeheonjeong <cogjs3210@naver.com>
2024-02-27 08:22:33 +0900

    feat: score, ranking finish

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import dbSetUp from "./db/dbSetUp.js";
 import cookieParser from "cookie-parser";
 import qasRouter from "./router/qasRouter.js";
 import bojRouter from "./router/bojRouter.js";
+import scoreRouter from "./router/scoreRouter.js";
 
 // 환경 변수 사용
 dotenv.config();
@@ -29,6 +30,7 @@ app.use(
 app.use("/api/users", userRouter);
 app.use("/api/qas", qasRouter);
 app.use("/api/bojs", bojRouter);
+app.use("/api/score", scoreRouter);
 
 // 에러 핸들링 미들웨어 -> 앱 전체적으로 검사하므로 맨 마지막에 배치해야함
 app.use(errorHandler);
