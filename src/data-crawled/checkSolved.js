e56dd7e9cb80977c2dcdfb4c35b5e357a8a1b4dd
Dongwon KIm <dongwon000103@gmail.com>
2024-02-28 08:24:29 +0900

    feat: playground

--- src/data-crawled/checkSolved.js
+++ src/data-crawled/checkSolved.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
-import { JSDOM } from 'jsdom';
+import axios from "axios";
+import { JSDOM } from "jsdom";
 
 export const checkSolved = async (problemNum, backjoonId) => {
-    const url = `https://www.acmicpc.net/status?from_mine=1&problem_id=${problemNum}&user_id=${backjoonId}`;
+  const url = `https://www.acmicpc.net/status?from_mine=1&problem_id=${problemNum}&user_id=${backjoonId}`;
 
-    const isSolved = axios
-        .get(url, {
-            headers: {
-                Accept: 'application/json',
-                'User-Agent':
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-            },
-        })
-        .then((response) => {
-            const htmlSnippet = response.data;
-            const dom = new JSDOM(htmlSnippet);
-            const document = dom.window.document;
+  const isSolved = axios
+    .get(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+      },
+    })
+    .then((response) => {
+      const htmlSnippet = response.data;
+      const dom = new JSDOM(htmlSnippet);
+      const document = dom.window.document;
 
-            const resultElements = document.querySelectorAll('.result-text');
+      const resultElements = document.querySelectorAll(".result-text");
 
-            for (const resultElement of resultElements) {
-                if (resultElement.textContent.includes('맞았습니다!!')) {
-                    return true;
-                }
-            }
-            return false;
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            return false;
-        });
+      for (const resultElement of resultElements) {
+        if (resultElement.textContent.includes("맞았습니다!!")) {
+          return true;
+        }
+      }
+      return false;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      return false;
+    });
 
-    return isSolved;
+  return isSolved;
 };
